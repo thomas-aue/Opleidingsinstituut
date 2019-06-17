cd1e7fccd0cc6b76d556bfc11ffe727bb4ea84ec
Thomas Aue <51162572+thomas-aue@users.noreply.github.com>
2019-06-17 10:53:01 +0200

    Add project files.

---
+++ Opleidingsinstituut/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Opleidingsinstituut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxSch = new System.Windows.Forms.ComboBox();
+            this.comboBoxMon = new System.Windows.Forms.ComboBox();
+            this.comboBoxPra = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxSch
+            // 
+            this.comboBoxSch.FormattingEnabled = true;
+            this.comboBoxSch.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBoxSch.Location = new System.Drawing.Point(143, 117);
+            this.comboBoxSch.Name = "comboBoxSch";
+            this.comboBoxSch.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSch.TabIndex = 0;
+            this.comboBoxSch.Text = "0";
+            // 
+            // comboBoxMon
+            // 
+            this.comboBoxMon.FormattingEnabled = true;
+            this.comboBoxMon.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBoxMon.Location = new System.Drawing.Point(143, 144);
+            this.comboBoxMon.Name = "comboBoxMon";
+            this.comboBoxMon.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMon.TabIndex = 1;
+            this.comboBoxMon.Text = "0";
+            // 
+            // comboBoxPra
+            // 
+            this.comboBoxPra.FormattingEnabled = true;
+            this.comboBoxPra.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBoxPra.Location = new System.Drawing.Point(143, 171);
+            this.comboBoxPra.Name = "comboBoxPra";
+            this.comboBoxPra.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPra.TabIndex = 2;
+            this.comboBoxPra.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Schriftelijk";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mondeling";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Praktisch";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(143, 42);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(121, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Geboortedatum";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Aantal vakken";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(143, 71);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.checkBox1.Size = new System.Drawing.Size(154, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Bemiddeling Arbeidsbureau";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(93, 221);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Bedrag:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(143, 221);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "€0,-";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Bereken";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 268);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxPra);
+            this.Controls.Add(this.comboBoxMon);
+            this.Controls.Add(this.comboBoxSch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Opleidingsinstituut";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxSch;
+        private System.Windows.Forms.ComboBox comboBoxMon;
+        private System.Windows.Forms.ComboBox comboBoxPra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Opleidingsinstituut/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Opleidingsinstituut
+{
+    public partial class Form1 : Form
+    {
+        int sch = 0;
+        int mon = 0;
+        int pra = 0;
+        double totaal = 0.0;
+        int years = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            sch = int.Parse(comboBoxSch.Text);
+            mon = int.Parse(comboBoxMon.Text);
+            pra = int.Parse(comboBoxPra.Text);
+            int aantalVakken = sch + mon + pra;
+
+            double lesgeld = sch * 50 + mon * 150 + pra * 150;            
+            
+            if (aantalVakken > 4)
+            {
+                lesgeld *= 0.95;
+            }
+
+            double studiemateriaal = sch * 50 + mon * 50 + pra * 125;
+            if (checkBox1.Checked)
+            {
+                studiemateriaal = 0;
+            }            
+
+            totaal = lesgeld + studiemateriaal;
+
+            years = DateTime.Now.Year - dateTimePicker1.Value.Year;
+            if (dateTimePicker1.Value.AddYears(years) > DateTime.Now)
+            {
+                years--;
+            }
+                       
+            if (years < 19)
+            {
+                totaal *= 0.98;
+            }
+
+            label7.Text = "€" + Math.Round(totaal, 2);
+        }
+    }
+}
